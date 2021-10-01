fdaabc1ab87f0720f541b0e3b1cf55583bdc1b57
ehageucla <91640372+ehageucla@users.noreply.github.com>
2021-10-01 14:54:22 -0700

    updatetest

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-print("hi")
-#if __name__ == '_main_' :
-#   x = "ECE_180_DA_DB"
-#   if x == "ECE_180_DA_DC":
-#       print("You are living in 2017")
-#   else:
-#      x = x + " - Best class ever"
-#        print(x)
+print(__name__)
+if __name__ == '_main_' :
+    x = "ECE_180_DA_DB"
+    if x == "ECE_180_DA_DC":
+        print("You are living in 2017")
+    else:
+        x = x + " - Best class ever"
+        print(x)
